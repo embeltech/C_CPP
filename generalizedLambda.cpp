c70e97bfb751e86a785269be1e6e9fcc253eb43b
embeltech <56225547+embeltech@users.noreply.github.com>
2024-07-07 13:25:58 +0530

    Update generalizedLambda.cpp

--- generalizedLambda.cpp
+++ generalizedLambda.cpp
@@ -2,7 +2,11 @@
 #include <iostream> 
 #include <string> 
 
-using namespace std; 
+int setValue(int x)
+{
+    return x*x;
+}
+
 int main() 
 { 
 
@@ -10,13 +14,25 @@ int main()
 	auto sum = [](auto a, auto b) { return a + b; }; 
 
 	// Find sum of two integers 
-	cout << sum(1, 6) << endl; 
+	std::cout << sum(1, 6) << std::endl; 
 
 	// Find sum of two floating numbers 
-	cout << sum(1.0, 5.6) << endl; 
+	std::cout << sum(1.0, 5.6) << std::endl; 
 
 	// Find sum of two strings 
-	cout << sum(string("TTL "), string("BMW")) << endl; 
+	std::cout << sum(std::string("TTL "), std::string("BMW")) << std::endl; 
 
+    //---------------------------------------------------
+    //generalised lambda capture. Captured once when lambda is declared. No need of datatype for captured variable.
+    auto iter = [counter = setValue(10)]()mutable { return ++counter;};
+    
+    int value = 0;
+    //range for loop
+    for(int i : {1,2,3,4,5})
+    {
+        value = iter();
+        std::cout<<i<<":"<<value<<std::endl;
+    }
+    
 	return 0; 
 } 
