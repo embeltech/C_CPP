dd7034845d270f0a38eba1b18fda750a37ef4a55
embeltech <56225547+embeltech@users.noreply.github.com>
2024-07-09 21:21:44 +0530

    Update unique_ptr.cpp

--- SmartPointers/unique_ptr.cpp
+++ SmartPointers/unique_ptr.cpp
@@ -1,119 +1,72 @@
 #include <iostream>
+#include <memory> // For shared_ptr
 
-template<typename T>
-class unique_ptr {
-private:
-    T* mPtr;
-
-public:
-    // Constructor
-    explicit unique_ptr(T* ptr = nullptr) : mPtr(ptr) {
-         std::cout<<"CTOR\n";
-    }
-
-    // Destructor
-    ~unique_ptr() {
-        if(mPtr)
-        {
-            delete mPtr;
-            mPtr = nullptr;
-            
-        }
-        std::cout<<"DTOR\n";
-    }
-    
-    // Disable copy operations
-    unique_ptr(const unique_ptr&) = delete;
-    unique_ptr& operator=(const unique_ptr&) = delete;
-
-    // Dereferencing operator
-    T& operator*() const {
-        return *mPtr;
-    }
-
-    // Member access operator
-    T* operator->() const {
-        return mPtr;
-    }
-
-    // Get raw pointer
-    T* get() const {
-        return mPtr;
-    }
-
-    // Move constructor
-    unique_ptr(unique_ptr&& other) noexcept : mPtr(other.mPtr) {
-        //no need to delete mPtr as object is under construction so mPtr does not point to any object
-        other.mPtr = nullptr;
-        std::cout<<"Move CTOR\n";
-    }
-
-    // Move assignment operator
-    void operator=(unique_ptr&& other) noexcept {
-        if (this != &other) {
-            delete mPtr;
-            mPtr = other.mPtr;
-            other.mPtr = nullptr;
-            std::cout<<"Move assignment\n";
-        }
-    }
-
-    // Check if the pointer is null
-    explicit operator bool() const {
-        std::cout<<"bool "<<(mPtr != nullptr)<<"\n";
-        return bool(mPtr != nullptr);
-    }
-    
-    void reset(T* ptr=nullptr)
-    {
-        if(mPtr)
-        {
-            delete mPtr;
-            mPtr = ptr;
-            std::cout<<"reset\n";
-        }
-    }
-    T* release()
-    {
-        T* temp = nullptr;
-        if(mPtr)
-        {
-            temp = mPtr;
-            mPtr = nullptr;
-            std::cout<<"release\n";
-        }
-        return temp;
-    }
-        
+class Rectangle{
+    std::string strName;
+    int iLength,iBreadth;
+    public:
+      Rectangle(std::string name, int x, int y):strName(name),iLength(x),iBreadth(y)
+      {
+          std::cout<<"Created Rectangle : "<<strName<<" ["<<iLength<<","<<iBreadth<<"]\n";
+      }
+      void print()
+      {
+          std::cout<<"Rectangle : "<<strName<<" ["<<iLength<<","<<iBreadth<<"]\n";
+      }
+      void set(int x, int y)
+      {
+        iLength=x;
+        iBreadth=y;
+      }
+      ~Rectangle()
+      {   
+        std::cout<<"Object deleted : "<<strName<<std::endl;
+      }
 };
 
-int main() {
-    unique_ptr<int> ptr1(new int(10));
-    std::cout << "*ptr1: " << *ptr1 << std::endl;
-
-    unique_ptr<int> ptr2(std::move(ptr1));
-    std::cout << "*ptr2: " << *ptr2 << std::endl;
-
-    if(!ptr1)
+int main()
+{
+    std::cout<<"Hello World\n";
+    Rectangle R1("R1",1,2);
+    R1.print();
+    //make_unique creates completely new object on heap and initializes with value of R1
+    std::unique_ptr<Rectangle> uptr1 = std::make_unique<Rectangle>(R1);
+    R1.set(3,4);
+    R1.print();
+    uptr1->print();
+//-----------------------------------------------------------------------  
+    auto uptr2 = std::unique_ptr<Rectangle>(new Rectangle ("R2",5,6));
+    uptr2->print();
+    //auto sptr3=sptr2; error as unique pointer cannot be assigned to other variable
+    auto uptr3 = std::move(uptr2);  //after moving sptr loses all ownership and becomes NULL
+    uptr3->print();
+    if(uptr2)
     {
-        std::cout<<"ptr1 is null\n";
-        ptr1=std::move(ptr2);
-        std::cout << "*ptr1: " << *ptr1 << std::endl;
+        std::cout<<"uptr2 is not NULL\n";
+    }
+    else{
+        std::cout<<"uptr2 is NULL\n";
+    }
+//----------------------------------------------------------------------- 
+    uptr3.reset(); //pointer becomes null. Object is deleted as sptr3 is only pointer points to object R2
+//-----------------------------------------------------------------------
+    auto uptr4 = std::unique_ptr<Rectangle>(new Rectangle("R3",10,20));
+    uptr4.release();
+    if(uptr4)
+    {
+        std::cout<<"After Release uptr4. uptr4 is not NULL.\n";
     }
     else
     {
-        std::cout<<"else";
+        std::cout<<"After uptr4.release() uptr4 is NULL.Object not deleted .The caller is responsible for cleaning up the object \n";
     }
-    ptr1.reset();
-    
-    unique_ptr<int> ptr3(new int(1000));
-    unique_ptr<int> ptr4(new int(10000));
-    std::cout << "*ptr3: " << *ptr3 << std::endl;
-    std::cout << "*ptr4: " << *ptr4 << std::endl;
-    
-    ptr3.reset(ptr4.release());
-    std::cout << "*ptr3: " << *ptr3 << std::endl;
-    
-
+  //-----------------------------------------------------------------------
+    std::unique_ptr<Rectangle> uptr5(new Rectangle("R4",10,20));
+    uptr5->print();
+    //resets uptr5. R5 will be created and sptr5 will point to R5.object R4 will get deleted as only pointer to R3 is getting reset
+    uptr5.reset(new Rectangle("R5",10,20));
+    uptr5->print();
+//-----------------------------------------------------------------------
+    std::cout<<"Main exiting\n";
     return 0;
 }
