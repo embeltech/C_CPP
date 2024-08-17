e63d787766de96d8b4fb4510c1928398f741224b
embeltech <56225547+embeltech@users.noreply.github.com>
2024-08-17 11:21:09 +0530

    Update objectSlicing.cpp

--- oops/objectSlicing.cpp
+++ oops/objectSlicing.cpp
@@ -1,69 +1,99 @@
 #include <iostream>
 
-class Robot
+class base
 {
-public:
-  Robot ()
-  {
-	std::cout << "Robot::Robot\n";
-  }
-
-  virtual void trace () const
-  {
-	std::cout << "Robot::trace\n";
-  }
-  virtual ~ Robot ()
-  {
-	std::cout << "Robot::~Robot\n";
-  }
+    public:
+    base ()
+    {
+        std::cout << "base::base\n";
+    }
+    virtual void print () const
+    {
+        std::cout << "base::print\n";
+    }
+    virtual ~ base ()
+    {
+        std::cout << "base::~base\n";
+    }
 };
 
-class Hexapod:public Robot
+class derived:public base
 {
-public:
-  Hexapod ()
-  {
-	std::cout << "Hexapod::Hexapod\n";
-  }
-  void trace () const override
-  {
-	std::cout << "Hexapod::trace\n";
-  }
-   ~Hexapod ()
-  {
-	std::cout << "Hexapod::~Hexapod\n";
-  }
+    public:
+    derived ()
+    {
+        std::cout << "derived::derived\n";
+    }
+    void print () const override
+    {
+        std::cout << "derived::print\n";
+    }
+    ~derived ()
+    {
+        std::cout << "derived::~derived\n";
+    }
 };
 
-void
-f (const Robot & p)
+void test1 (const base & p)
 {
-  p.trace ();
+    std::cout << "-- test1 --\n";
+    p.print ();
+    std::cout<<"-----------\n";
 }
 
-void
-g (const Robot p)
+void test2 (const base p)
 {
-  p.trace ();
+    std::cout << "-- test2 --\n";
+    p.print ();
+    std::cout<<"-----------\n";
 }
 
-void h (Robot* p)
+void test3 (base* p)
 {
-  p->trace ();
+    std::cout << "-- test3 --\n";
+    p->print ();
+    std::cout<<"-----------\n";
 }
 
 
 int main ()
 {
-  std::cout << "Start\n";
-  Hexapod lwr;
-  f (lwr);
-  g (lwr);
-  
-
-  Robot *robot = new Hexapod ();
-  h (robot);
-  delete robot;
-  
-  std::cout << "End\n";
+    std::cout << "Start\n";
+    derived derivedObj;
+    test1 (derivedObj);
+    test2 (derivedObj);
+    
+    std::cout<<"Create basePtr to store derived obj\n";
+    base *basePtr = new derived ();
+    test3 (basePtr);
+    std::cout<<"Delete basePtr\n";
+    delete basePtr;
+    
+    std::cout << "End\n";
 }
+
+/*----------------------------- output -------------------------------------
+Start
+base::base
+derived::derived
+-- test1 --
+derived::print
+-----------
+-- test2 --
+base::print
+-----------
+base::~base
+Create basePtr to store derived obj
+base::base
+derived::derived
+-- test3 --
+derived::print
+-----------
+Delete basePtr
+derived::~derived
+base::~base
+End
+derived::~derived
+base::~base
+
+*/
