e858ada6026a999276b0923d3a7d259ceb4e40e7
embeltech <56225547+embeltech@users.noreply.github.com>
2025-01-28 09:08:04 +0530

    Update lambda.cpp

--- lambda.cpp
+++ lambda.cpp
@@ -15,3 +15,36 @@ int main()
 
     return 0;
 }
+
+/*********************************************************************************************************************
+
+Capture by Value: 
+- Captures a copy of the variable.
+- By default case of capture.
+- Capture takes place once at the time of declaration of lambda. 
+- Any change in captured variable after declaration of lambda will not affect captured value inside lambda
+
+Capture by Reference: 
+- Captures a reference to the variable
+- Any change in captured variable even after declaration of lambda will affect captured value inside lambda
+
+Capture All by Value: 
+- Captures all variables in the enclosing scope by value.
+- auto lambda = [=]() { body of lambda };
+
+Capture All by Reference: 
+- Captures all variables in the enclosing scope by reference
+- auto lambda = [&]() { body of lambda };
+
+Mixed Capture
+- Capture x by value and y by reference.
+- auto lambda = [x,&y]() { body of lambda };
+
+- Capture x by value and all remaining by reference.
+- auto lambda = [x,&]() { body of lambda };
+
+- Capture x by reference and all remaining by value.
+- auto lambda = [&x,=]() { body of lambda };
+
+
+*/
