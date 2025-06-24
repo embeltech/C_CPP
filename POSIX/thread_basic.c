46e1565cbe53245308970247a28dd7d2eabf398b
embeltech <56225547+embeltech@users.noreply.github.com>
2025-06-24 11:36:36 +0530

    Create thread_basic.c

---
+++ POSIX/thread_basic.c
@@ -0,0 +1,44 @@
+/*
+*   C compiler: gcc -o out thread_basic.c -lpthread
+*   C++ compiler: g++ -o out thread_basic.c -lpthread
+*/
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h> //sleep
+
+void * threadFunc(void * arg)
+{
+    int * id = (int *) arg;
+    int i = 0;
+    for(i=0; i<=20; i++)
+    {
+        printf("Thread %d| i=%d\n",*id,i);        
+        usleep(100000);
+    }
+    pthread_exit(NULL);
+}
+
+int main()
+{
+     pthread_t thread1, thread2;
+     int id1 = 1;
+     int id2 = 2;
+     int  iret1, iret2;
+
+    /* Create independent threads each of which will execute function */
+
+     iret1 = pthread_create( &thread1, NULL, threadFunc, (void*) &id1);
+     iret2 = pthread_create( &thread2, NULL, threadFunc, (void*) &id2);
+
+     printf("Thread 1 returns: %d\n",iret1);
+     printf("Thread 2 returns: %d\n",iret2);
+    
+     /* Wait till threads are complete before main continues. Unless we  */
+     /* wait we run the risk of executing an exit which will terminate   */
+     /* the process and all threads before the threads have completed.   */
+
+     pthread_join( thread1, NULL);
+     pthread_join( thread2, NULL); 
+    
+     return 0;
+}
